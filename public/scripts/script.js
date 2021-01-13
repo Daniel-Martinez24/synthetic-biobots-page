0c60d89bb706bdeb81611b053b3c341dd451f139
Daniel Martinez Diaz <dan24diaz@gmail.com>
2021-01-13 15:12:40 -0600

    Añadido react

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,5 @@
-// Aquí se añade el JavaScript principal
+// Aquí se añade el JavaScript principalt 
+// import { Tarjetas } from '../components/tarjeta.js';
+const Tarjetas = <h1>Hello, world</h1>;
+
+ReactDOM.render(Tarjetas, document.getElementById('tarjetas'));
