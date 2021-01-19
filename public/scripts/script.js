a9fec0bdf0eb167bab6ffe790dcc41507ef95ec4
Daniel Martinez Diaz <dan24diaz@gmail.com>
2021-01-19 14:16:29 -0600

    Hacer que el menu mobile aparezca y desaparezca

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,17 @@
 const menu = document.getElementById("menu");
-menu.addEventListener("click", function(){ headerAparecer(); });
+const header = document.getElementsByClassName("header-right");
+const link_a_secciones = header[0].getElementsByTagName("a");
 
 const headerAparecer = () => {
     const header = document.getElementsByTagName("header");
-    header[0].style.display == 'none' ?
+    header[0].style.display == 'block' ?
     header[0].style.display = 'none' : header[0].style.display = 'block';
 }
+
+menu.addEventListener("click", function(){ headerAparecer(); });
+
+if (screen.width < 600) {
+    Object.values(link_a_secciones).map(links => links.addEventListener("click", () => {headerAparecer();} ));
+}
+
+// console.log(Object.values(link_a_secciones));
